5ddb7512c3aa165cfe5234bb87dacb9ce41224ce
Vasyl Nabochenko <vasyl@bankjoy.com>
2023-09-06 15:46:56 +0300

    UC#7 Create complex unit tests

--- StudentsUseCase7/StudentsUseCase7/Program.cs
+++ StudentsUseCase7/StudentsUseCase7/Program.cs
@@ -49,3 +49,39 @@ public class StudentConverter : IStudentConverter
         }).ToList();
     }
 }
+
+public class Player
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Experience { get; set; }
+    public List<int> Skills { get; set; }
+}
+
+
+public class PlayerAnalyzer : IPlayerAnalyzer
+{
+    public double CalculateScore(List<Player> players)
+    {
+        double score = 0;
+
+        foreach (var player in players)
+        {
+            double skillsAverage = player.Skills.Sum() / (double)player.Skills.Count;
+            double contribution = player.Age * player.Experience * skillsAverage;
+
+            if (player.Age < 18)
+            {
+                contribution *= 0.5;
+            }
+
+            if (player.Experience > 10)
+            {
+                contribution *= 1.2;
+            }
+            score += contribution;
+        }
+
+        return score;
+    }
+}
---
+++ StudentsUseCase7/StudentsUseCase7Tests/PlayerAnalyzerTests.cs
@@ -0,0 +1,104 @@
+namespace StudentsUseCase7Tests
+{
+    public class PlayerAnalyzerTests
+    {
+        private readonly IPlayerAnalyzer sut;
+        public PlayerAnalyzerTests()
+        {
+            sut = new PlayerAnalyzer();
+        }
+
+        [Fact]
+        public void Normal_Player()
+        {
+            //Arange
+            var testPlayers = new List<Player>() 
+            { 
+                new Player(){ Age = 25,Experience=5, Skills = new List<int>() {2, 2, 2 } },
+            };
+
+            //Act
+            var result = sut.CalculateScore(testPlayers);
+
+            //Assert
+            Assert.Equal(250, result);
+        }
+
+        [Fact]
+        public void Junior_Player()
+        {
+            //Arange
+            var testPlayers = new List<Player>()
+            {
+                new Player(){ Age = 15,Experience = 3, Skills = new List<int>() {3, 3, 3 } },
+            };
+
+            //Act
+            var result = sut.CalculateScore(testPlayers);
+
+            //Assert
+            Assert.Equal(67.5, result);
+        }
+
+        [Fact]
+        public void Senior_Player()
+        {
+            //Arange
+            var testPlayers = new List<Player>()
+            {
+                new Player(){ Age = 35,Experience = 15, Skills = new List<int>() {4, 4, 4 } },
+            };
+
+            //Act
+            var result = sut.CalculateScore(testPlayers);
+
+            //Assert
+            Assert.Equal(2520, result);
+        }
+
+        [Fact]
+        public void Multiple_Players()
+        {
+            //Arange
+            var testPlayers = new List<Player>()
+            {
+                new Player(){ Age = 35,Experience = 15, Skills = new List<int>() {4, 4, 4 } },
+                new Player(){ Age = 25,Experience=5, Skills = new List<int>() {2, 2, 2 } }
+            };
+
+            //Act
+            var result = sut.CalculateScore(testPlayers);
+
+            //Assert
+            Assert.Equal(2520 + 250, result);
+        }
+
+
+        [Fact]
+        public void Null_Skills()
+        {
+            //Arange
+            var testPlayers = new List<Player>()
+            {
+                new Player(){ Age = 35,Experience = 15 },
+            };
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => sut.CalculateScore(testPlayers));
+        }
+
+        [Fact]
+        public void Empty_Array()
+        {
+            //Arange
+            var testPlayers = new List<Player>();
+
+            //Act
+            var result = sut.CalculateScore(testPlayers);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+    }
+
+}
