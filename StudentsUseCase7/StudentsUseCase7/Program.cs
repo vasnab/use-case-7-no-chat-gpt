686dd0883814de13fec677592454ac22cae4d6e2
Vasyl Nabochenko <vasyl@bankjoy.com>
2023-09-06 13:47:17 +0300

    UC#7 Create unit tests

--- StudentsUseCase7/StudentsUseCase7/Program.cs
+++ StudentsUseCase7/StudentsUseCase7/Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Grade { get; set; }
+    public bool Exceptional { get; set; }
+    public bool HonorRoll { get; set; }
+    public bool Passed { get; set; }
+}
+
+public class StudentConverter : IStudentConverter
+{
+    public List<Student> ConvertStudents(List<Student> students)
+    {
+
+        return students.Select(student =>
+        {
+            var result = new Student
+            {
+                Name = student.Name,
+                Age = student.Age,
+                Grade = student.Grade
+            };
+
+            if (student.Grade > 90)
+            {
+                if (student.Age < 21)
+                {
+                    result.Exceptional = true;
+                }
+                else
+                {
+                    result.HonorRoll = true;
+                }
+            }
+            else if (student.Grade > 70)
+            {
+                result.Passed = true;
+            }
+            else
+            {
+                result.Passed = false;
+            }
+
+            return result;
+        }).ToList();
+    }
+}
---
+++ StudentsUseCase7/StudentsUseCase7Tests/StudentConverterTests.cs
@@ -0,0 +1,111 @@
+namespace StudentsUseCase7Tests
+{
+    public class StudentConverterTests
+    {
+        private readonly IStudentConverter sut;
+        public StudentConverterTests()
+        {
+            sut = new StudentConverter();
+        }
+
+        [Fact]
+        public void High_Achiever()
+        {
+            //Arange
+            var testStudents = new List<Student>() 
+            { 
+                new Student(){Name = "test", Age = 21, Grade = 91 },
+            };
+
+            //Act
+            var result = sut.ConvertStudents(testStudents);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Contains(result, x => x.HonorRoll);
+        }
+
+        [Fact]
+        public void Exceptional_Young_High_Achiever()
+        {
+            //Arange
+            var testStudents = new List<Student>()
+            {
+                new Student(){Name = "test", Age = 20, Grade = 91 },
+            };
+
+            //Act
+            var result = sut.ConvertStudents(testStudents);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Contains(result, x => x.Exceptional);
+        }
+
+        [Fact]
+        public void Passed_Student()
+        {
+            //Arange
+            // grade between 71 and 90 (inclusive)
+            var testStudents = new List<Student>()
+            {
+                new Student(){ Name = "test", Age = 20, Grade = 71 },
+                new Student(){ Name = "test", Age = 20, Grade = 90 }
+            };
+
+            //Act
+            var result = sut.ConvertStudents(testStudents);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.All(result, x => Assert.True(x.Passed));
+        }
+
+        [Fact]
+        public void Failed_Student()
+        {
+            //Arange
+            // grade between 71 and 90 (inclusive)
+            var testStudents = new List<Student>()
+            {
+                new Student(){ Name = "test", Age = 20, Grade = 70 },
+                new Student(){ Name = "test", Age = 20, Grade = 0 }
+            };
+
+            //Act
+            var result = sut.ConvertStudents(testStudents);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.All(result, x => Assert.False(x.Passed));
+        }
+
+        //Negative Test Cases
+        [Fact]
+        public void Empty_Array()
+        {
+            //Arange
+            // grade between 71 and 90 (inclusive)
+            var testStudents = new List<Student>();
+
+            //Act
+            var result = sut.ConvertStudents(testStudents);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Null_Array()
+        {
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => sut.ConvertStudents(null));
+        }
+
+    }
+}
